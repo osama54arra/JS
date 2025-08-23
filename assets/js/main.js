fa095557f1aa7e6003176228f156b0c4a4316c93
Osama <osama54arra@gmail.com>
2025-08-23 14:40:53 +0300

    2nd

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,59 +11,73 @@
 // window.console.log(a);
 // window.console.log(b);
 // window.console.log(a+b); 
-var passDB="12"
-var GPA=3.000;
-var emailDB="osama54arra@gmail.com"
-var email = window.prompt("Enter your Email");
-var passwoed = window.prompt("Enter your password");
-var msg=`welcom ya ${passDB} hello`;
-window.console.log(msg);
-document.ri
-// window.console.log(passwoed);
-if(emailDB==email){
-    if(passwoed == passDB)
-    {   window.console.log("welcome");
-        document.getElementById("table1").innerHTML="<tr><td>1</td><td>welcome Osama</td><td>osama54arra@gmail.com</td></tr>";
-    } 
-    else{
-        window.console.log("Invalid Password")
-        document.getElementById("table1").innerHTML="<tr><td></td><td>Invalid Password</td><td></td></tr>";
-    }
-}
-else{
-    window.console.log("خطأ حاول مرة اخرى")
-    document.getElementById("table1").innerHTML="<tr><td></td><td>خطأ في الاجابة</td><td></td></tr>";
-}
-var user = {
-    name:"osama",
-    age:20,
-    gender:'mail',
-    dep:{
-        name:'it',
-        number:54,
-    }
+
+
+
+
+// var passDB="12"
+// var GPA=3.000;
+// var emailDB="osama54arra@gmail.com"
+// var email = window.prompt("Enter your Email");
+// var passwoed = window.prompt("Enter your password");
+// var msg=`welcom ya ${passDB} hello`;
+// window.console.log(msg);
+// document.ri
+// // window.console.log(passwoed);
+// if(emailDB==email){
+//     if(passwoed == passDB)
+//     {   window.console.log("welcome");
+//         document.getElementById("table1").innerHTML="<tr><td>1</td><td>welcome Osama</td><td>osama54arra@gmail.com</td></tr>";
+//     } 
+//     else{
+//         window.console.log("Invalid Password")
+//         document.getElementById("table1").innerHTML="<tr><td></td><td>Invalid Password</td><td></td></tr>";
+//     }
+// }
+// else{
+//     window.console.log("خطأ حاول مرة اخرى")
+//     document.getElementById("table1").innerHTML="<tr><td></td><td>خطأ في الاجابة</td><td></td></tr>";
+// }
+// var user = {
+//     name:"osama",
+//     age:20,
+//     gender:'mail',
+//     dep:{
+//         name:'it',
+//         number:54,
+//     }
+// }
+
+// window.console.log(user['name'])
+// window.console.log(user.name)
+// window.console.log(user)
+//  user.salary=2500;
+//  console.log(user.salary)
+// window.console.log(user['salary']).nnerHTML=msg;
+// window.console.log(user)
+// window.console.log(user.dep)
+
+// kye=[
+//     {
+//       "userId": 1,
+//       "id": 1,
+//       "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+//       "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+//     },
+//     {
+//       "userId": 1,
+//       "id": 2,
+//       "title": "qui est esse",
+//       "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+//     }
+// ];   
+
+
+
+function developerOsama() {
+   document.querySelector("wwe").textContent = 
+   "the developer is niger Osama";
 }
 
-window.console.log(user['name'])
-window.console.log(user.name)
-window.console.log(user)
- user.salary=2500;
- console.log(user.salary)
-window.console.log(user['salary'])nnerHTML=msg;
-window.console.log(user)
-window.console.log(user.dep)
 
-kye=[
-    {
-      "userId": 1,
-      "id": 1,
-      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-      "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-    },
-    {
-      "userId": 1,
-      "id": 2,
-      "title": "qui est esse",
-      "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-    }
-];   
+
